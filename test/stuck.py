06310c9396ca14c3f0e98802e04e66ca2150048b
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-06-28 16:57:26 +0900

    テスト用スタック判定作りましたby 大島

---
+++ test/stuck.py
@@ -0,0 +1,35 @@
+from time import sleep
+from gpiozero import Motor
+import BMC050
+import sys
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/6-axis')
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Motor')
+
+
+def stuck(thd=1):
+    BMC050.bmc050_setup()
+    motor = Motor(Rpin1, Rpin2)
+    while 1:
+        motor.forward(0.2)
+        sleep(0.3)
+        accdata = BMC050.acc_data()
+        acc_x = accdata[0]
+        acc_y = accdata[1]
+        acc_z = accdata[2]
+        acc = (acc_x**2 + acc_y**2 + acc_z**2)**0.5
+        if acc < thd:
+            print('スタックした　：　'+str(acc))
+            motor.stop()
+            break
+        else:
+            motor.forward(0.2)
+            sleep(2)
+
+
+if __name__ == '__main__':
+    Rpin1 = 17
+    Rpin2 = 18
+
+    Lpin1 = 19
+    Lpin2 = 20
+    stuck(12)
