8c29d2d050acb7c7506adc2dbbeb821228598aee
yoppi <oosima@kimura-lab.net>
2021-07-01 22:25:53 +0900

    motor関数追加したよ。これ動作するか早めに確認したい～by 大島

--- test/motorTest.py
+++ test/motorTest.py
@@ -1,5 +1,7 @@
 from gpiozero import Motor
 from time import sleep
+import sys
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Communication')
 
 Rpin1 = 17
 Rpin2 = 18
@@ -18,6 +20,7 @@ def motor_move(strength_r, strength_l, time):
     if strength_r >= 0 & strength_l >= 0:
         motor_r = Motor(Rpin1, Rpin2)
         motor_l = Motor(Lpin1, Lpin2)
+        Xbee.str_trans('前進開始')
         motor_r.forward(strength_r)
         motor_l.forward(strength_l)
         sleep(time)
@@ -25,6 +28,7 @@ def motor_move(strength_r, strength_l, time):
     elif strength_r < 0 & strength_l < 0:
         motor_r = Motor(Rpin1, Rpin2)
         motor_l = Motor(Lpin1, Lpin2)
+        Xbee.str_trans('後退開始')
         motor_r.backward(abs(strength_r))
         motor_l.backward(abs(strength_l))
         sleep(time)
@@ -32,6 +36,7 @@ def motor_move(strength_r, strength_l, time):
     elif strength_r >= 0 & strength_l < 0:
         motor_r = Motor(Rpin1, Rpin2)
         motor_l = Motor(Lpin1, Lpin2)
+        Xbee.str_trans('右回転開始')
         motor_r.forkward(abs(strength_r))
         motor_l.backward(abs(strength_l))
         sleep(time)
@@ -39,12 +44,16 @@ def motor_move(strength_r, strength_l, time):
     elif strength_r < 0 & strength_l >= 0:
         motor_r = Motor(Rpin1, Rpin2)
         motor_l = Motor(Lpin1, Lpin2)
+        Xbee.str_trans('左回転開始')
         motor_r.backward(abs(strength_r))
         motor_l.forkward(abs(strength_l))
         sleep(time)
 
 
 motor_move(0.5, 0.5, 10)
+motor_move(-0.5, -0.5, 10)
+motor_move(0.5, -0.5, 10)
+motor_move(-0.5, 0.5, 10)
 
 
 motor = Motor(Rpin1, Rpin2)
