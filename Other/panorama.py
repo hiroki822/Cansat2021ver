c35e19e2b78bb2caa510c9eaf756331b95e53685
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-04 19:47:46 +0900

    パノラマ合成関数変更した(エラー確認したい)
    パノラマ合成用の関数を修正した。
    エラー確認したいかな

--- Other/panorama.py
+++ Other/panorama.py
@@ -4,17 +4,25 @@ import glob
 import time
 
 
-def panorama(srcdir,prefex='',file='.jpeg'):
-    srcfilecount = len(glob.glob1(srcdir + '/', '*'+file))
-    resultcount = len(glob.glob1('result/', srcdir+'*.jpeg'))
+def panorama(srcdir,prefix='',srcext='.jpg',dstext='.jpg'):
+    """
+    パノラマを合成するための関数
+    ソースディレクトリ内に合成用の写真を番号をつけて入れておく。（例：IMG0.jpg,IMG1.jpg）
+    srcdir:ソースディレクトリ
+    prefix:番号の前につける文字
+    srcext:ソースの拡張子
+    dstext:できたものの拡張子
+    """
+    srcfilecount = len(glob.glob1(srcdir + '/', '*'+srcext))
+    resultcount = len(glob.glob1('result/', srcdir, '*'+dstext))
     photos = []
 
     for i in range(0, srcfilecount):
-        photos.append(cv2.imread(srcdir +'/' + prefex + str(i) + file))
+        photos.append(cv2.imread(srcdir +'/' + prefix + str(i) + srcext))
 
     stitcher = cv2.Stitcher.create(0)
     status, result = stitcher.stitch(photos)
-    cv2.imwrite('result/' + srcdir + '-' + str(resultcount) + '.jpeg', result)
+    cv2.imwrite('result/' + srcdir + '-' + str(resultcount) + srcext, result)
 
     if status == 0:
         print("success")
@@ -22,14 +30,12 @@ def panorama(srcdir,prefex='',file='.jpeg'):
         print('failed')
 
 
-
-
-stratTime = time.time()  # プログラムの開始時刻
-
-# 処理
-panorama(srcdir='nisho-ground12_640×480',prefex='',file='.jpg')
-
-endTime = time.time()
-runTime = endTime - stratTime
-
-print(runTime)
+if __name__ == "__main__":
+    try:
+        tratTime = time.time()  # プログラムの開始時刻
+        panorama(srcdir)
+        endTime = time.time() #プログラムの終了時間
+        runTime = endTime - stratTime
+        print(runTime)
+    except:
+        print('Error')
