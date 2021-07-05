78bd8376c2dd748c7182290849a30ddb38f3b570
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-07-05 18:06:21 +0900

    スタックavoidが終わればGPS走行慣性

--- SensorModule/Motor/motor.py
+++ SensorModule/Motor/motor.py
@@ -1,5 +1,13 @@
 from gpiozero import Motor
 from time import sleep
+import BMC050
+import stuck
+import Xbee
+import sys
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/6-axis')
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Motor')
+sys.path.append('/home/pi/desktop/Cansat2021ver/SensorModule/Communication')
+
 # ピン番号は仮
 Rpin1 = 17
 Rpin2 = 18
@@ -8,18 +16,52 @@ Lpin1 = 19
 Lpin2 = 20
 
 
-def motor_move(strength_r, strength_l, time):
+def motor_stop():
     Rpin1 = 19
     Rpin2 = 26
     Lpin1 = 5
     Lpin2 = 6
-    # 前進
+    motor_r = Motor(Rpin1, Rpin2)
+    motor_l = Motor(Lpin1, Lpin2)
+    motor_r.stop()
+    motor_l.stop()
+
+
+def motor_move(strength_l, strength_r, time):
+    Rpin1 = 19
+    Rpin2 = 26
+    Lpin1 = 5
+    Lpin2 = 6
+    # 前進するときのみスタック判定
     if strength_r >= 0 & strength_l >= 0:
         motor_r = Motor(Rpin1, Rpin2)
         motor_l = Motor(Lpin1, Lpin2)
         motor_r.forward(strength_r)
         motor_l.forward(strength_l)
-        sleep(time)
+        sleep(0.2)
+
+        bool_stuck = stuck.stuck_jud()
+        if bool_stuck:
+            Xbee.str_trans('スタックしました一回目')
+            motor_r.stop()
+            motor_l.stop()
+            # 一応もう一回スタック判定確かめる
+            motor_r.forward(strength_r)
+            motor_l.forward(strength_l)
+            sleep(0.2)
+            bool_stuck = stuck.stuck_jud()
+            if bool_stuck:
+                Xbee.str_trans('スタックしました二回目')
+                motor_r.stop()
+                motor_l.stop()
+                # スタック回避関数ぶち込む
+            else:
+                pass
+        else:
+            Xbee.str_trans('直進中')
+            sleep(time-0.2)
+            motor_r.stop()
+            motor_l.stop()
     # 後進
     elif strength_r < 0 & strength_l < 0:
         motor_r = Motor(Rpin1, Rpin2)
@@ -27,6 +69,8 @@ def motor_move(strength_r, strength_l, time):
         motor_r.backward(abs(strength_r))
         motor_l.backward(abs(strength_l))
         sleep(time)
+        motor_r.stop()
+        motor_l.stop()
     # 右回転
     elif strength_r >= 0 & strength_l < 0:
         motor_r = Motor(Rpin1, Rpin2)
@@ -34,6 +78,8 @@ def motor_move(strength_r, strength_l, time):
         motor_r.forkward(abs(strength_r))
         motor_l.backward(abs(strength_l))
         sleep(time)
+        motor_r.stop()
+        motor_l.stop()
     # 左回転
     elif strength_r < 0 & strength_l >= 0:
         motor_r = Motor(Rpin1, Rpin2)
@@ -41,6 +87,53 @@ def motor_move(strength_r, strength_l, time):
         motor_r.backward(abs(strength_r))
         motor_l.forkward(abs(strength_l))
         sleep(time)
+        motor_r.stop()
+        motor_l.stop()
+
+
+# スタックavoid関数用です
+def motor_move_avoid(strength_l, strength_r, time):
+    Rpin1 = 19
+    Rpin2 = 26
+    Lpin1 = 5
+    Lpin2 = 6
+    # 前進するときのみスタック判定
+    if strength_r >= 0 & strength_l >= 0:
+        motor_r = Motor(Rpin1, Rpin2)
+        motor_l = Motor(Lpin1, Lpin2)
+        motor_r.forward(strength_r)
+        motor_l.forward(strength_l)
+        sleep(time)
+        motor_r.stop()
+        motor_l.stop()
+
+    # 後進
+    elif strength_r < 0 & strength_l < 0:
+        motor_r = Motor(Rpin1, Rpin2)
+        motor_l = Motor(Lpin1, Lpin2)
+        motor_r.backward(abs(strength_r))
+        motor_l.backward(abs(strength_l))
+        sleep(time)
+        motor_r.stop()
+        motor_l.stop()
+    # 右回転
+    elif strength_r >= 0 & strength_l < 0:
+        motor_r = Motor(Rpin1, Rpin2)
+        motor_l = Motor(Lpin1, Lpin2)
+        motor_r.forkward(abs(strength_r))
+        motor_l.backward(abs(strength_l))
+        sleep(time)
+        motor_r.stop()
+        motor_l.stop()
+    # 左回転
+    elif strength_r < 0 & strength_l >= 0:
+        motor_r = Motor(Rpin1, Rpin2)
+        motor_l = Motor(Lpin1, Lpin2)
+        motor_r.backward(abs(strength_r))
+        motor_l.forkward(abs(strength_l))
+        sleep(time)
+        motor_r.stop()
+        motor_l.stop()
 
 
 motor = Motor(Rpin1, Rpin2)
