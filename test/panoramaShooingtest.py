1d4c37296b0de7e15c9f88f3fcc78bee8d3a675b
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-06 13:32:40 +0900

    Create panoramaShooingtest.py

---
+++ test/panoramaShooingtest.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('/home/pi/Desktop/Cansat2021ver/Other')
+sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Camera')
+
+import panorama
+import Capture
+
+
+def shooting(magx_off, magy_off):
+    """
+    パノラマ撮影用の関数
+    引数は磁気のオフセット
+    """
+    magdata = BMC050.mag_dataRead()
+    magx = magdata[0]
+    magy = magdata[1]
+    preθ = Calibration.calculate_angle_2D(magx, magy, magx_off, magy_off)
+    sumθ = 0
+    deltaθ = 0
+    Xbee.str_trans('whileスタート preθ:{0}'.format(preθ))
+
+    while sumθ <= 360:
+        Captrue.Capture('/home/pi/Desktop/Cansat2021ver/photosotorage/panorama')
+        motorTest.motor_move(-1, 1, 1) #調整するところ？
+        magdata = BMC050.mag_dataRead()
+        magx = magdata[0]
+        magy = magdata[1]
+        latestθ = Calibration.calculate_angle_2D(magx, magy, magx_off, magy_off)
+        if preθ >= 300 and latestθ <= 100:
+            latestθ += 360
+        deltaθ = preθ - latestθ
+        sumθ += deltaθ
+        if latestθ >= 360:
+            latestθ -= 360
+        preθ = latestθ
+        Xbee.str_trans('sumθ:', sumθ, ' preθ:', preθ, ' deltaθ:', deltaθ)
