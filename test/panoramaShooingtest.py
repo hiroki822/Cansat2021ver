3f55548c8942dbb339ace7f28ced4ccd87760d7f
Koji1116 <58416025+Koji1116@users.noreply.github.com>
2021-07-07 13:26:42 +0900

    1

--- test/panoramaShooingtest.py
+++ test/panoramaShooingtest.py
@@ -3,13 +3,15 @@ sys.path.append('/home/pi/Desktop/Cansat2021ver/Other')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Camera')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Emvironmental')
 sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Communication')
+sys.path.append('/home/pi/Desktop/Cansat2021ver/SensorModule/Motor')
 import panorama
 import Capture
 import BMC050
 import Xbee
+import motor
 
 
-def shooting(magx_off, magy_off, path):
+def panorama_shooting(magx_off, magy_off, path):
     """
     パノラマ撮影用の関数
     引数は磁気のオフセット
@@ -24,11 +26,26 @@ def shooting(magx_off, magy_off, path):
 
     while sumθ <= 360:
         Captrue.Capture(path)
-        motorTest.motor_move(-1, 1, 1) #調整するところ？
+        motor.move(-1, 1, 1) #調整するところ？
         magdata = BMC050.mag_dataRead()
         magx = magdata[0]
         magy = magdata[1]
         latestθ = Calibration.calculate_angle_2D(magx, magy, magx_off, magy_off)
+        
+        #------Stuck------#
+        if latestθ - preθ <= 10:
+            Xbee.str_trans('Stuck')
+            motor.move(1, 1, 5)
+            #----Initialize-----#
+            magdata = BMC050.mag_dataRead()
+            magx = magdata[0]
+            magy = magdata[1]
+            preθ = Calibration.calculate_angle_2D(magx, magy, magx_off, magy_off)
+            sumθ = 0
+            deltaθ = 0
+            Xbee.str_trans('whileスタート preθ:{0}'.format(preθ))
+            continue
+        
         if preθ >= 300 and latestθ <= 100:
             latestθ += 360
         deltaθ = preθ - latestθ
