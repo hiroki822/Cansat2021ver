49acdb0fe93add2f1dc4c2328d8c53426674e68e
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2021-07-08 13:28:20 +0900

    GPS走行改良。関数作ったりしたby oosima

--- test/runnningGPS_yoppi.py
+++ test/runnningGPS_yoppi.py
@@ -37,10 +37,61 @@ def timer(t):
     cond = False
 
 
+def adjust_direction():
+    """
+    方向調整
+    """
+    global theta
+    Xbee.str_trans('theta = '+str(theta)+'---回転調整開始！')
+    count = 0
+    while abs(theta) > 30:
+        Xbee.str_trans(str(count))
+        if count > 8:
+            Xbee.str_trans('スタックもしくはこの場所が適切ではない')
+            stuck.stuck_avoid()
+
+        if abs(theta) <= 180:
+            if abs(theta) <= 60:
+                Xbee.str_trans('theta = '+str(theta)+'---回転開始ver1')
+                motor.motor_move(
+                    np.sign(theta)*0.5, -1*np.sign(theta)*0.5, 3)
+                motor.stop()
+
+            elif abs(theta) <= 180:
+                Xbee.str_trans('theta = '+str(theta)+'---回転開始ver2')
+                motor.motor_move(-np.sign(theta)
+                                 * 0.5, np.sign(theta)*0.5, 3)
+                motor.motor_stop()
+        elif abs(theta) > 180:
+            if abs(theta) >= 300:
+                Xbee.str_trans('theta = '+str(theta)+'---回転開始ver3')
+                motor.motor_move(-np.sign(theta)
+                                 * 0.5, np.sign(theta)*0.5, 5)
+                motor.motor_stop()
+            elif abs(theta) > 180:
+                Xbee.str_trans('theta = '+str(theta)+'---回転開始ver4')
+                motor.motor_move(
+                    np.sign(theta)*0.5, -np.sign(theta)*0.5, 5)
+                motor.motor_stop()
+        count += 1
+        data = Calibration.get_data()
+        magx = data[0]
+        magy = data[1]
+        # --- 0 <= θ <= 360 ---#
+        theta = Calibration.calculate_angle_2D(
+            magx, magy, magx_off, magy_off)
+        direction = Calibration.calculate_direction(lon2, lat2)
+        azimuth = direction["azimuth1"]
+        theta = theta-azimuth
+
+    Xbee.str_trans('theta = '+str(theta)+'---回転終了!!!')
+
+
 if __name__ == "__main__":
     BMC050.BMC050_setup()
     GPS.openGPS()
     print('Run Phase Start!')
+    Xbee.str_trans('GPS走行開始')
     # --- difine goal latitude and longitude ---#
     lon2 = 139.90833592590076
     lat2 = 35.91817558805946
@@ -59,7 +110,7 @@ if __name__ == "__main__":
         magx_off = magdata_offset[3]
         magy_off = magdata_offset[4]
         magz_off = magdata_offset[5]
-        time.sleep(1)
+
         # --- calculate θ ---#
         data = Calibration.get_data()
         magx = data[0]
@@ -67,74 +118,8 @@ if __name__ == "__main__":
         # --- 0 <= θ <= 360 ---#
         θ = Calibration.calculate_angle_2D(magx, magy, magx_off, magy_off)
         # ------------- rotate contorol -------------#
-        Calibration.rotate_control(θ, lon2, lat2)
-        location = Stuck.stuck_detection1()
-        longitude_past = location[0]
-        latitude_past = location[1]
-        # --- initialize count ---#
-        loop_count = 0
-        stuck_count = 0
 
-        # --- calculate  goal direction ---#
-        direction = Calibration.calculate_direction(lon2, lat2)
-        goal_distance = direction["distance"]
-        print('goal distance =' + str(goal_distance))
-        if goal_distance <= 15:
-            break
-
-        data = Calibration.get_data()
-        magx = data[0]
-        magy = data[1]
-       # --- 0 <= θ <= 360 ---#
-        theta = Calibration.calculate_angle_2D(
-            magx, magy, magx_off, magy_off)
-        azimuth = direction["azimuth1"]
-        theta = theta-azimuth
-
-        # ローバーを目標地点に合わせる
-        # パラメータ要確認
-        Xbee.str_trans('theta = '+str(theta)+'---回転調整開始！')
-        count = 0
-        while abs(theta) > 30:
-            Xbee.str_trans(str(count))
-            if count > 6:
-                Xbee.str_trans('スタックもしくはこの場所が適切ではない')
-                stuck.stuck_avoid()
-
-            if abs(theta) <= 180:
-                if abs(theta) <= 60:
-                    Xbee.str_trans('theta = '+str(theta)+'---回転開始ver1')
-                    motor.motor_move(
-                        np.sign(theta)*0.5, -1*np.sign(theta)*0.5, 3)
-                    motor.stop()
-
-                elif abs(theta) <= 180:
-                    Xbee.str_trans('theta = '+str(theta)+'---回転開始ver2')
-                    motor.motor_move(-np.sign(theta)
-                                     * 0.5, np.sign(theta)*0.5, 3)
-                    motor.motor_stop()
-            elif abs(theta) > 180:
-                if abs(theta) >= 300:
-                    Xbee.str_trans('theta = '+str(theta)+'---回転開始ver3')
-                    motor.motor_move(-np.sign(theta)
-                                     * 0.5, np.sign(theta)*0.5, 5)
-                    motor.motor_stop()
-                elif abs(theta) > 180:
-                    Xbee.str_trans('theta = '+str(theta)+'---回転開始ver4')
-                    motor.motor_move(
-                        np.sign(theta)*0.5, -np.sign(theta)*0.5, 5)
-                    motor.motor_stop()
-            count += 1
-            data = Calibration.get_data()
-            magx = data[0]
-            magy = data[1]
-            # --- 0 <= θ <= 360 ---#
-            theta = Calibration.calculate_angle_2D(
-                magx, magy, magx_off, magy_off)
-            azimuth = direction["azimuth1"]
-            theta = theta-azimuth
-
-        Xbee.str_trans('theta = '+str(theta)+'---回転終了!!!')
+        adjust_direction()
 
         # パラメータ要確認
         Xbee.str_trans('theta = '+str(theta)+'---直進開始')
@@ -142,7 +127,15 @@ if __name__ == "__main__":
         if stuck.stuck_duj():
             stuck.stuck_avoid()
         motor.motor_move(0.8, 0.8, 10)
+        motor.stop()
 
-     # --- calculate  goal direction ---#
+        # --- calculate  goal direction ---#
         direction = Calibration.calculate_direction(lon2, lat2)
         goal_distance = direction["distance"]
+        if goal_distance >= 15:
+            Xbee.str_trans('goal distance =' +
+                           str(goal_distance)+'----GPS走行続く')
+        else:
+            Xbee.str_trans('goal distance =' +
+                           str(goal_distance)+'----GPS走行終了！')
+        print('goal distance =' + str(goal_distance))
